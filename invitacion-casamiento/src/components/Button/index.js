fd53002a15c20c28146c4c166f3a9267740a4208
unknown <guillermo.sequeira@wolox.com.ar>
2021-03-19 19:56:02 -0300

    apply changes

--- invitacion-casamiento/src/components/Button/index.js
+++ invitacion-casamiento/src/components/Button/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import styles from './Button.module.scss'
 
-const Button = ({ href, text }) => (
-  <a href={href} className={styles.button}>{text}</a>
+const Button = ({ href, text, onClick }) => (
+  <a
+    href={href}
+    className={styles.button}
+    onClick={e => {
+      if (!href) {
+        e.preventDefault();
+        onClick();
+      }
+    }}>
+    {text}
+  </a>
 )
+
+Button.defaultProps = {
+  onClick: () => { }
+}
 
 export default Button;
--- invitacion-casamiento/src/components/Infowedding/ConfirmedAttendance/index.js
+++ invitacion-casamiento/src/components/Infowedding/ConfirmedAttendance/index.js
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
 import Button from '../../Button';
 import styles from './ConfirmedAttendance.module.scss';
 
-const ConfirmedAttendance = () => (
-  <div className={styles.container}>
-    <h2 className={styles.title} >TE ESPERAMOS</h2>
-    <span className={styles.description}>Para que formes parte de esta gran celebracion ¡Confirmanos tu asistencia!</span>
-    <Button text="CONFIRMAR ASISTENCIA" />
-    <span className={styles.thanks}>"GRACIAS POR FORMAR PARTE DE ESTA AVENTURA"</span>
-    <span className={styles.names}>Ani y Guille</span>
-  </div>
-)
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  }
+};
+
+const ConfirmedAttendance = () => {
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  const openModal = () => {
+    setIsOpen(true);
+  }
+
+  const closeModal = () => {
+    setIsOpen(false);
+  }
+
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title} >TE ESPERAMOS</h2>
+      <span className={styles.description}>Para que formes parte de esta gran celebracion ¡Confirmanos tu asistencia!</span>
+      <Button text="CONFIRMAR ASISTENCIA" onClick={openModal} />
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        className={styles.modal}
+        overlayClassName={styles.overlay}
+        ariaHideApp={false}
+        shouldFocusAfterRender={false}
+      >
+        <h2>¿A quien te gustaria confirmarle tu asistencia?</h2>
+        <Button text="A GUILLE" onClick={closeModal}>close</Button>
+        <Button text="A ANI" onClick={closeModal}>close</Button>
+        <Button text="CANCELAR" onClick={closeModal}>close</Button>
+      </Modal>
+      <span className={styles.thanks}>"GRACIAS POR FORMAR PARTE DE ESTA AVENTURA"</span>
+      <span className={styles.names}>Ani y Guille</span>
+    </div>
+  )
+}
 
 export default ConfirmedAttendance;

3f7da263a62e96f0ca5847626ef1ecf4f176ff2b
unknown <guillermo.sequeira@wolox.com.ar>
2021-11-17 02:20:29 -0300

    fixes

--- invitacion-casamiento/src/App.js
+++ invitacion-casamiento/src/App.js
@@ -1,5 +1,8 @@
-import Carousel from './components/Carousel';
-import InfoWedding from './components/Infowedding';
+import Carousel from "./components/Carousel";
+import InfoWedding from "./components/Infowedding";
+import "lazysizes";
+// import a plugin
+import "lazysizes/plugins/attrchange/ls.attrchange";
 
 function App() {
   return (
--- invitacion-casamiento/src/components/Carousel/index.js
+++ invitacion-casamiento/src/components/Carousel/index.js
@@ -4,6 +4,7 @@ import Arrow from "./components/Arrow";
 import styles from "./Carousel.module.scss";
 import Dots from "./components/Dots";
 import Info from "./components/Info";
+import asset1 from "../../assets/foto1.jpg";
 /* eslint-disable no-debugger */
 
 const CarouselComponent = () => {
@@ -19,22 +20,22 @@ const CarouselComponent = () => {
         itemsToScroll={1}
         itemsToShow={1}
       >
-        <div className={styles.containerItem}>
+        <div className={`lazyload ${styles.containerItem}`}>
           <div className={styles.gradientOverlay} />
         </div>
-        <div className={styles.containerItem2}>
+        <div className={`lazyload ${styles.containerItem2}`}>
           <div className={styles.gradientOverlay} />
         </div>
-        <div className={styles.containerItem3}>
+        <div className={`lazyload ${styles.containerItem3}`}>
           <div className={styles.gradientOverlay} />
         </div>
-        <div className={styles.containerItem4}>
+        <div className={`lazyload ${styles.containerItem4}`}>
           <div className={styles.gradientOverlay} />
         </div>
-        <div className={styles.containerItem5}>
+        <div className={`lazyload ${styles.containerItem5}`}>
           <div className={styles.gradientOverlay} />
         </div>
-        <div className={styles.containerItem6}>
+        <div className={`lazyload ${styles.containerItem6}`}>
           <div className={styles.gradientOverlay} />
         </div>
       </Carousel>
